9819a7a54dd2de6b325ad90a26a12bd33a116b82
jolanValteau <j.valteau@glutton-stone.studio>
2025-06-22 21:03:53 +0200

    Added Props and random spawner

--- Assets/Scripts/Level/LevelGenerator.cs
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -9,112 +9,138 @@ using Random = UnityEngine.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public static LevelGenerator Instance;
+	public static LevelGenerator Instance;
 
-    public UnityEvent OnLevelCompleted;
-    
-    /// <summary>
-    /// 0 for random.
-    /// </summary>
-    [SerializeField] int seed = 0;
-    [SerializeField] Transform cameraTest;
+	public UnityEvent OnLevelCompleted;
+	
+	/// <summary>
+	/// 0 for random.
+	/// </summary>
+	[SerializeField] int seed = 0;
+	[SerializeField] Transform cameraTest;
 
-    List<Room> loadedRooms = new();
-    List<List<Room>> staticBags = new();
-    List<List<Room>> currentBags = new();
-    public List<Room> spawnedRooms = new();
-    System.Random bagsRnd;
-    int currentRoomId = -1;
-    private Vector3 nextPos;
-    [SerializeField] Vector3 cameraOffset;
+	List<Room> loadedRooms = new();
+	List<List<Room>> staticBags = new();
+	List<List<Room>> currentBags = new();
+	public List<Room> spawnedRooms = new();
+	System.Random bagsRnd;
+	int currentRoomId = -1;
+	private Vector3 nextPos;
+	[SerializeField] Vector3 cameraOffset;
 
-    const int bagsAmount = 2;
+	List<GameObject> props = new();
 
-    private void Awake()
-    {
-        if (Instance == null)
-            Instance = this;
-    }
+	const int bagsAmount = 2;
 
-    void Start()
-    {
-        loadedRooms = Resources.LoadAll<Room>("Rooms/PlayableRooms").ToList();
-        nextPos = cameraTest.position;
+	private void Awake()
+	{
+		if (Instance == null)
+			Instance = this;
+	}
 
-        InitBags();
-        SpawnNextRoom();
-    }
+	void Start()
+	{
+		loadedRooms = Resources.LoadAll<Room>("Rooms/PlayableRooms").ToList();
+		nextPos = cameraTest.position;
 
-    private void Update()
-    {
-        if (Keyboard.current.kKey.wasPressedThisFrame)
-            SpawnNextRoom();
+		props = Resources.LoadAll<GameObject>("Props").ToList();
 
-        
-        cameraTest.transform.position = Vector3.Lerp(cameraTest.transform.position, nextPos, Time.deltaTime * 10);
-    }
+		InitBags();
+		SpawnNextRoom();
+	}
 
-    void InitBags()
-    {
-        // create/get seed
-        if (seed == 0) 
-            seed = Random.Range(0, int.MaxValue);
-        System.Random rnd = new System.Random(seed);
-        bagsRnd = new System.Random(seed);
+	private void Update()
+	{
+		if (Keyboard.current.kKey.wasPressedThisFrame)
+			SpawnNextRoom();
 
-        // generate bags
-        staticBags.Clear();
-        List<Room> shuffledRooms = loadedRooms.OrderBy(x => rnd.Next()).ToList();
+		
+		cameraTest.transform.position = Vector3.Lerp(cameraTest.transform.position, nextPos, Time.deltaTime * 10);
+	}
 
-        int sliceSize = loadedRooms.Count() / bagsAmount;
-        for (int bagIndex = 0; bagIndex < bagsAmount; bagIndex++)
-        {
-            // get shuffled slice
-            int sliceStart = sliceSize * bagIndex;
-            staticBags.Add(loadedRooms.GetRange(sliceStart, sliceSize).OrderBy(x => rnd.Next()).ToList());
-        }
-    }
+	void InitBags()
+	{
+		// create/get seed
+		if (seed == 0) 
+			seed = Random.Range(0, int.MaxValue);
+		System.Random rnd = new System.Random(seed);
+		bagsRnd = new System.Random(seed);
 
-    public void SpawnNextRoom()
-    {
-        currentRoomId++;
+		// generate bags
+		staticBags.Clear();
+		List<Room> shuffledRooms = loadedRooms.OrderBy(x => rnd.Next()).ToList();
 
-        // refill bags
-        if (currentBags.Count == 0)
-        {
-            currentBags = new(staticBags);
-            for (int bagIndex = 0; bagIndex < currentBags.Count(); bagIndex++)
-                currentBags[bagIndex] = currentBags[bagIndex].OrderBy(x => bagsRnd.Next()).ToList();
-        }
+		int sliceSize = loadedRooms.Count() / bagsAmount;
+		for (int bagIndex = 0; bagIndex < bagsAmount; bagIndex++)
+		{
+			// get shuffled slice
+			int sliceStart = sliceSize * bagIndex;
+			staticBags.Add(loadedRooms.GetRange(sliceStart, sliceSize).OrderBy(x => rnd.Next()).ToList());
+		}
+	}
 
-        // get next room
-        Room nextRoom = currentBags[0][0];
-        currentBags[0].RemoveAt(0);
-        if (currentBags[0].Count == 0)
-            currentBags.RemoveAt(0);
+	public void SpawnNextRoom()
+	{
+		currentRoomId++;
 
-        Vector3 newRoomPos = spawnedRooms.Count > 0
-            ? Vector3.forward * (spawnedRooms[^1].transform.position.z + spawnedRooms[^1].roomLength)
-            : Vector3.forward * 5; //TODO c'est super moche mais tant pis... 5 = taille de la firstRoom 
-        Room newRoom = Instantiate(nextRoom, newRoomPos, Quaternion.identity);
-        spawnedRooms.Add(newRoom);
+		// refill bags
+		if (currentBags.Count == 0)
+		{
+			currentBags = new(staticBags);
+			for (int bagIndex = 0; bagIndex < currentBags.Count(); bagIndex++)
+				currentBags[bagIndex] = currentBags[bagIndex].OrderBy(x => bagsRnd.Next()).ToList();
+		}
 
-        // remove previous rooms
-        while (spawnedRooms.Count() > 4)
-        {
-            Destroy(spawnedRooms[0].gameObject);
-            spawnedRooms.RemoveAt(0);
-        }
-        
-        // cam pos
-        if (spawnedRooms.Count != 1) // Disable cam movement for the first level
-        {
-            int roomWidth = spawnedRooms[^2].GetRoomWidth() * 2 + 1;
-            int lvlOffset = spawnedRooms[^2].roomLength > roomWidth
-                ? spawnedRooms[^2].roomLength
-                : roomWidth;
-            
-            nextPos = new Vector3(cameraOffset.x, cameraOffset.y + lvlOffset*1.2f, cameraOffset.z - lvlOffset/2f) + spawnedRooms[^2].transform.position;
-        }
-    }
+		// get next room
+		Room nextRoom = currentBags[0][0];
+		currentBags[0].RemoveAt(0);
+		if (currentBags[0].Count == 0)
+			currentBags.RemoveAt(0);
+
+		Vector3 newRoomPos = spawnedRooms.Count > 0
+			? Vector3.forward * (spawnedRooms[^1].transform.position.z + spawnedRooms[^1].roomLength)
+			: Vector3.forward * 5; //TODO c'est super moche mais tant pis... 5 = taille de la firstRoom 
+		Room newRoom = Instantiate(nextRoom, newRoomPos, Quaternion.identity);
+		spawnedRooms.Add(newRoom);
+
+		// remove previous rooms
+		while (spawnedRooms.Count() > 4)
+		{
+			Destroy(spawnedRooms[0].gameObject);
+			spawnedRooms.RemoveAt(0);
+		}
+		
+		// cam pos
+		if (spawnedRooms.Count != 1) // Disable cam movement for the first level
+		{
+			int roomWidth = spawnedRooms[^2].GetRoomWidth() * 2 + 1;
+			int lvlOffset = spawnedRooms[^2].roomLength > roomWidth
+				? spawnedRooms[^2].roomLength
+				: roomWidth;
+			
+			nextPos = new Vector3(cameraOffset.x, cameraOffset.y + lvlOffset*1.2f, cameraOffset.z - lvlOffset/2f) + spawnedRooms[^2].transform.position;
+		}
+
+		// spawn props
+		
+		foreach (Tile tile in newRoom.GetAllTiles())
+		{
+			if (tile.model == Tile.Model.Floor && tile.triggerType == "None")
+			{
+				float randomPick = Random.Range(0, 100);
+				int count = randomPick < 85 ? 0 : randomPick < 90 ? 1 : randomPick < 95 ? 2 : 3;
+				for (int i = 0; i < count; i++)
+				{
+					GameObject prop = Instantiate(props[Random.Range(0, props.Count)]);
+					prop.transform.SetParent(tile.transform);
+
+					float randomRange = 0.4f;
+					prop.transform.localPosition = new Vector3(Random.Range(-randomRange, randomRange), 0.5f, Random.Range(-randomRange, randomRange));
+					Vector3 rot = prop.transform.localEulerAngles;
+					rot.y = Random.Range(0, 360);
+					prop.transform.localEulerAngles = rot;
+				}
+			}
+		}
+	}
 }
