8d414c9b6959378c11b18c051fb2818411c59532
Erwan <46385057+LaLoutreCosmique@users.noreply.github.com>
2025-06-23 15:52:26 +0200

    reset score button

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] Button m_PlayButton;
+    [SerializeField] Button m_ResetButton;
     [SerializeField] LoadingScreen m_LoadingScreen;
 
     private void Start()
     {
         m_PlayButton.onClick.AddListener(Play);
+        m_ResetButton.onClick.AddListener(ResetScore);
     }
 
     void Play()
     {
         m_LoadingScreen.LoadScene("Scenes/Game");
+    }
+
+    void ResetScore()
+    {
+        PlayerPrefs.SetInt("BEST_SCORE", 0);
     }
 }
