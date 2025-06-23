b4677d1d49d63c5247fa34a149217b8cc538ca60
jolanValteau <j.valteau@glutton-stone.studio>
2025-06-23 10:42:53 +0200

    Player script change folder fix merge

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -16,13 +15,49 @@ public class Player : MonoBehaviour
 	[HideInInspector] public float BallRadius;
 	[SerializeField] private LayerMask m_IgnoreLayersRay;
 
+	[SerializeField] private LayerMask groundLayer;
+	[SerializeField] private float groundRaycastSize;
+
 	[SerializeField] private Transform VFX;
 
+
+	private bool grounded;
+
+	[Header("Reglages du son")]
+	public float speedThreshold = 0.1f;
+	public float maxVolume = 1.0f;
+	public float baseVolumeMultiplier = 0.7f;
+	public float volumeSmoothing = 5f;
+	public float groundCheckDistance = 0.6f;
+
+	[Header("Vent")]
+	public float windMinVolume = 0.15f;
+	public float windBaseVolume = 0.2f;
+	public float windMaxVolume = 0.4f;
+
+	private AudioSource rollingAudio;
+	private AudioSource windAudio;
+
+	[Header("Impact")]
+	[SerializeField] private AudioClip[] wallImpactClips;
+	[SerializeField] private GameObject impactVFXPrefab;
+	[SerializeField] private float impactVolume = 0.8f;
+	[SerializeField] private string wallTag = "Wall";
+	[SerializeField] private string pillarTag = "Pillar";
+	[SerializeField] private float impactSpeedThreshold = 2.5f;
+	[SerializeField] private float impactCooldown = 0.3f;
+	private float lastImpactTime = -Mathf.Infinity;
 
 	private void Start()
 	{
 		m_PlayerRb = GetComponent<Rigidbody>();
 		BallRadius = GetComponent<SphereCollider>().radius;
+
+		rollingAudio = SoundsManager.Instance.audioSourceRoll;
+		windAudio = SoundsManager.Instance.audioSourceWind;
+
+		rollingAudio.volume = 0f;
+		windAudio.volume = 0f;
 
 		GameManager.Instance.OnPlayerDie += Die;
 	}
@@ -31,6 +66,7 @@ public class Player : MonoBehaviour
 	{
 		GameManager.Instance.OnPlayerDie -= Die;
 	}
+
 	private void FixedUpdate()
 	{
 		Move();
@@ -41,15 +77,87 @@ public class Player : MonoBehaviour
 		}
 	}
 
+	private void Update()
+	{
+		CheckGrounded();
+
+		float speed = m_PlayerRb.GetPointVelocity(transform.position).magnitude;
+
+		if (grounded && speed > speedThreshold)
+		{
+			if (!rollingAudio.isPlaying)
+				rollingAudio.Play();
+
+			float targetRollVolume = Mathf.Clamp01(speed / 10f) * maxVolume * baseVolumeMultiplier;
+			rollingAudio.volume = Mathf.Lerp(rollingAudio.volume, targetRollVolume, Time.deltaTime * volumeSmoothing);
+		}
+		else
+		{
+			rollingAudio.volume = Mathf.Lerp(rollingAudio.volume, 0f, Time.deltaTime * volumeSmoothing);
+			if (rollingAudio.volume < 0.01f && rollingAudio.isPlaying)
+				rollingAudio.Stop();
+		}
+
+		float windTargetVolume;
+
+		if (grounded)
+		{
+			windTargetVolume = Mathf.Clamp(
+				windMinVolume + (speed / 10f) * (windMaxVolume - windMinVolume),
+				windMinVolume,
+				windMaxVolume
+			);
+		}
+		else
+		{
+			windTargetVolume = windMinVolume;
+		}
+
+		if (!windAudio.isPlaying)
+			windAudio.Play();
+
+		windAudio.volume = Mathf.Lerp(windAudio.volume, windTargetVolume, Time.deltaTime * volumeSmoothing);
+
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (!collision.collider.CompareTag(wallTag) && !collision.collider.CompareTag(pillarTag))
+			return;
+
+		float currentTime = Time.time;
+		float impactSpeed = m_PlayerRb.GetPointVelocity(transform.position).magnitude;
+
+		if (impactSpeed < impactSpeedThreshold)
+			return;
+
+		if (currentTime - lastImpactTime < impactCooldown)
+			return;
+
+		lastImpactTime = currentTime;
+
+		if (wallImpactClips.Length > 0)
+		{
+			AudioClip clip = wallImpactClips[UnityEngine.Random.Range(0, wallImpactClips.Length)];
+			AudioSource.PlayClipAtPoint(clip, collision.contacts[0].point, impactVolume);
+		}
+
+		// VFX
+		//if (impactVFXPrefab != null)
+		//{
+		//    ContactPoint contact = collision.contacts[0];
+		//    Quaternion rotation = Quaternion.LookRotation(contact.normal);
+		//    Instantiate(impactVFXPrefab, contact.point, rotation);
+		//}
+	}
+
 	public void Die()
 	{
 		m_PlayerRb.isKinematic = true;
-
 	}
 
 	void Move()
 	{
-		Vector3 movement = new Vector3(moveDirection.x, 0, moveDirection.y * 1.5f/*(1 + slider.value)*/);
+		Vector3 movement = new Vector3(moveDirection.x, 0, moveDirection.y * 1.5f);
 
 		fallForce = Vector3.down * m_FallForce;
 		Ray ray = new Ray(transform.position, Vector3.down);
@@ -59,6 +167,7 @@ public class Player : MonoBehaviour
 		}
 
 		m_PlayerRb.linearVelocity = Vector3.Lerp(m_PlayerRb.linearVelocity, (movement + fallForce) * m_Speed, m_Acceleration * Time.fixedDeltaTime);
+
 		if (m_PlayerRb.linearVelocity.magnitude <= 0.01f)
 		{
 			m_PlayerRb.linearVelocity = Vector3.zero;
@@ -66,51 +175,8 @@ public class Player : MonoBehaviour
 		}
 	}
 
-	void SetVFXTransform(Vector3 movement)
+	void CheckGrounded()
 	{
-		VFX.localRotation = Quaternion.Euler(m_PlayerRb.linearVelocity);
-		//      if (movement == Vector3.zero)
-		//{
-		//}
-		//else if (movement == new Vector3(0, 0, 1.5f))
-		//{
-		//	VFX.localScale = Vector3.one;
-		//          VFX.localRotation = Quaternion.Euler(0, 0, 0);
-		//      }
-		//else if (movement == new Vector3(0, 0, -1.5f))
-		//      {
-		//          VFX.localScale = Vector3.one;
-		//          VFX.localRotation = Quaternion.Euler(0, 180, 0);
-		//      }
-		//      else if (movement == new Vector3(1, 0, 0))
-		//      {
-		//          VFX.localScale = new Vector3(.5f, .5f, .5f);
-		//          VFX.localRotation = Quaternion.Euler(0, 90, 0);
-		//      }
-		//      else if (movement == new Vector3(-1, 0, 0))
-		//      {
-		//          VFX.localScale = new Vector3(.5f, .5f, .5f);
-		//          VFX.localRotation = Quaternion.Euler(0, 270, 0);
-		//      }
-		//      else if (movement == new Vector3(.71f, 0, 1.06f))
-		//      {
-		//          VFX.localRotation = Quaternion.Euler(0, 0, 45);
-		//          VFX.localScale = new Vector3(.5f, .5f, .5f);
-		//      }
-		//      else if (movement == new Vector3(-.71f, 0, 1.06f))
-		//      {
-		//          VFX.localRotation = Quaternion.Euler(0, 0, 315);
-		//          VFX.localScale = new Vector3(.5f, .5f, .5f);
-		//      }
-		//      else if (movement == new Vector3(.71f, 0, -1.06f))
-		//      {
-		//          VFX.localRotation = Quaternion.Euler(0, 0, 135);
-		//          VFX.localScale = new Vector3(.5f, .5f, .5f);
-		//      }
-		//      else if (movement == new Vector3(-.71f, 0, -1.06f))
-		//      {
-		//          VFX.localRotation = Quaternion.Euler(0, 0, 225f);
-		//          VFX.localScale = new Vector3(.5f, .5f, .5f);
-		//      }
+		grounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
 	}
 }
