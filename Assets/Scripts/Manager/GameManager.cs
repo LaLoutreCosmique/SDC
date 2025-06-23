375b58694ea5218f7591f2c0e934239e7bc0187d
jolanValteau <j.valteau@glutton-stone.studio>
2025-06-23 14:47:54 +0200

    Missing stuff of Audio

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -80,12 +80,14 @@ public class GameManager : MonoBehaviour
     public void SetDay()
     {
         RenderSettings.skybox = m_DaySkybox;
-        m_DayLight.SetActive(true);
+        if (m_DayLight)
+            m_DayLight.SetActive(true);
     }
 
     public void SetNight()
     {
         RenderSettings.skybox = m_NightSkybox;
-        m_DayLight.SetActive(false);
+		if (m_DayLight)
+			m_DayLight.SetActive(false);
     }
 }
