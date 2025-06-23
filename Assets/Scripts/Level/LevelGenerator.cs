ff25ea550f4293badcd9764e139b0026ae0ab7fa
Bramzzrr <bramzz.frtn@gmail.com>
2025-06-23 22:58:25 +0200

    real fix restart

--- Assets/Scripts/Level/LevelGenerator.cs
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -49,18 +49,8 @@ public class LevelGenerator : MonoBehaviour
 	private void Awake()
 	{
 
-		if (Instance == null)
-		{
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
-		else
-		{
-			Destroy(Instance.gameObject);
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-	}
+        Instance = this;
+    }
 
 	void Start()
 	{
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -21,18 +21,8 @@ public class GameManager : MonoBehaviour
 
 	private void Awake()
 	{
-		if (Instance == null)
-		{
-			Instance = this;
-			DontDestroyOnLoad(gameObject);
-		}
-		else
-        {
-            Destroy(Instance.gameObject);
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-	}
+        Instance = this;
+    }
 
 	private void Start()
     {
